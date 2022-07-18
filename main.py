b1bc26e261238f8f864a75cbdbeadef04d6f340b
Olly <oliverwhancock@gmail.com>
2022-07-18 19:01:22 +0100

    basic framework 2

---
+++ main.py
@@ -0,0 +1,40 @@
+
+
+
+class Board:
+    def __init__(self):
+        pass
+    
+    def __str__(self):
+        pass
+
+    def make_move(self, move):
+        pass
+
+    def check_move(self):
+        pass
+
+
+class Player:
+    def __init__(self, name, side):
+        self.name = name
+        self.side = side
+
+    def get_turn():
+        pass
+
+
+def assign_player():
+    pass
+
+
+def intialise():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()

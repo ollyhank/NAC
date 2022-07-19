ffe501b276e3c4599b8d5c62aa7f0b2ee29b2d64
Olly <oliverwhancock@gmail.com>
2022-07-19 11:35:06 +0100

    Basic linting

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
-
+'''DOCSTRING'''
 
 
 class Board:
+    '''DOCSTRING'''
     def __init__(self):
         pass
-    
+
     def __str__(self):
         pass
 
     def make_move(self, move):
-        pass
+        '''DOCSTRING'''
 
     def check_move(self):
-        pass
+        '''DOCSTRING'''
 
 
 class Player:
+    '''DOCSTRING'''
     def __init__(self, name, side):
         self.name = name
         self.side = side
 
-    def get_turn():
-        pass
-
 
 def assign_player():
-    pass
+    '''DOCTSTRING'''
 
 
 def intialise():
-    pass
+    '''DOCSTRING'''
 
 
 def main():
-    pass
+    '''DOCSTRING'''
 
 
 if __name__ == '__main__':

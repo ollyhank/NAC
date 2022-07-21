911ffa78bf76c95a316f1a91b5778983b65f0779
Olly <oliverwhancock@gmail.com>
2022-07-21 09:44:52 +0100

    Board string in the works

--- main.py
+++ main.py
@@ -3,11 +3,23 @@
 
 class Board:
     '''DOCSTRING'''
+    board = [[None, None, None],[None, None, None],[None, None, None]]
+
     def __init__(self):
         pass
 
     def __str__(self):
-        pass
+        string = ""
+        seperator = "-------------------"
+        for rows in self.board:
+            single_row = " " + str(rows[0]) + " | " + str(rows[1]) + " | " + str(rows[1]) + "\n"
+            string += single_row
+        
+        string.insert(seperator + "\n")
+
+        string.removesuffix(seperator)
+        return string
+
 
     def make_move(self, move):
         '''DOCSTRING'''
@@ -27,12 +39,17 @@ def assign_player():
     '''DOCTSTRING'''
 
 
-def intialise():
+def initialise():
     '''DOCSTRING'''
+    game_board = Board()
+    return game_board
 
 
 def main():
     '''DOCSTRING'''
+    game_board = initialise()
+    print(game_board)
+    
 
 
 if __name__ == '__main__':
